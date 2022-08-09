fbe4dd72a8dd65e160572560628a357a0f392d63
Theodora Lopez <theodora.n.lopez@gmail.com>
2022-08-09 14:24:49 -0700

    Corrected script to update table data.

--- static/js/app.js
+++ static/js/app.js
@@ -29,16 +29,17 @@ var filters = {};
 // 3. Use this function to update the filters. 
 function updateFilters() {
 
-    // 4a. Save the element that was changed as a variable.
+  // 4a. Save the element that was changed as a variable.
   let changedElement = d3.select(this);
-    // 4b. Save the value that was changed as a variable.
+  
+  // 4b. Save the value that was changed as a variable.
   let elementValue = changedElement.property("value");
-  console.log(elementValue);
-    // 4c. Save the id of the filter that was changed as a variable.
+  
+  // 4c. Save the id of the filter that was changed as a variable.
   let filterId = changedElement.attr("id");
-  console.log(filterId);
-    // 5. If a filter value was entered then add that filterId and value
-    // to the filters list. Otherwise, clear that filter from the filters object.
+  
+  // 5. If a filter value was entered then add that filterId and value
+  // to the filters list. Otherwise, clear that filter from the filters object.
   if (elementValue) {
     filters[filterId] = elementValue;
   }
@@ -47,23 +48,18 @@ function updateFilters() {
   }
   
   // 6. Call function to apply all filters and rebuild the table
-  filterTable();
-  
+  filterTable(elementValue, filterId);
 };
   
 // 7. Use this function to filter the table when data is entered.
-function filterTable() {
-  
+function filterTable(elementValue, filterId) {
   // 8. Set the filtered data to the tableData.
   let filteredData = tableData;
   
   // 9. Loop through all of the filters and keep any data that
   // matches the filter values
-  filters.forEach((elementValue) => {
-    filteredData = filteredData.filter(filterId => filterId.changedElement === elementValue)
-  });
-    
-    
+  filteredData = filteredData.filter(element => element[filterId] === elementValue)
+
   //10. Finally, rebuild the table using the filtered data
   buildTable(filteredData); 
 };
